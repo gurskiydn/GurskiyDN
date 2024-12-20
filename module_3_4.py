fc251a67772f624170334eea5c96539b3d5f3abd
gurskiydn <gurskiydn@gmail.com>
2024-12-20 18:06:41 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,9 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []  # Создаем пустой список для подходящих слов
+    root_word_lower = root_word.lower()
     for word in other_words:# Перебираем все слова из других слов
-        if word.startswith(root_word):# Проверка, начинается ли текущее слово с корневого слова
-            same_words.append(word)  # Если да, добавляем его в список
+        word_lower = word.lower()
+        if root_word_lower in word_lower and word_lower.count (root_word_lower) == 1:
+            same_words.append(word)  #  добавляем его в список
     return same_words  # Вставляем в список подходящих слов
 # Вызов функции и вывод результата
-result = single_root_words('программ', 'программирование', 'программный', 'программа', 'документ')
+result = single_root_words('Программ', 'программирование', 'программный', 'Программа', 'документ')
 print(result)
